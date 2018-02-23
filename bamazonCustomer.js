bb926943167ceada4a36c836b769b868e702b7ca
anishbnair <nbanish@gmail.com>
2018-02-23 11:47:02 -0500

    included code to establish connection to mysql server and database

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,4 +1,7 @@
 
+
+require("dotenv").config();
+
 // load mysql npm package
 var mysql = require("mysql");
 // load inquirer npm package
@@ -6,10 +9,18 @@ var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
     host: "localhost",
-    port: 8899,
+    port: 3306,
 
     user: "root",
-    password: "",
-    database: bamazon_db
+    password: process.env.MYSQL_PWD,
+    database: "bamazon_db"
 
 });
+
+console.log(process.env.MYSQL_PWD);
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("connected to the database");
+})
