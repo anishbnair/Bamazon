2f2f2afc8c3d602f078f8bccd0f754154f4b94b4
anishbnair <nbanish@gmail.com>
2018-02-26 16:35:18 -0500

    updated files

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,9 +9,9 @@ var inquirer = require("inquirer");
 // load chalk npm package
 var chalk = require("chalk");
 
-var connection = mysql.createConnection({
+var db = mysql.createConnection({
     host: process.env.mysql_host,
-    port: process.env.mysql_port,
+    port: process.env.PORT || 3306,
     user: process.env.mysql_user,
     password: process.env.mysql_pwd,
     database: "bamazon_db"
@@ -19,12 +19,11 @@ var connection = mysql.createConnection({
 
 
 // connect to the mysql server and sql database
-connection.connect(function (err) {
+db.connect(function (err) {
     if (err) throw err;
-    console.log("connected to the database");
+    // console.log("connected to the database");
     // display items for sale if database connection is successful
     displayItemsForSale();
-    // buyItem();
 })
 
 
@@ -33,14 +32,15 @@ function displayItemsForSale() {
     console.log(chalk.magentaBright("\n****************** Welcome to Bamazon!!! ************************"));
     console.log(chalk.magentaBright("\n****************** Below are the items available for sale: ******\n"));
 
-    connection.query("SELECT * FROM products", function (error, result) {
+    db.query("SELECT * FROM products", function (error, result) {
         if (error) throw error;
         // log all items from database
         // console.log(result);
-        console.log(chalk.greenBright("Item ID: " + "    || Product Name: " + "                    || Price: "));
+        console.log(chalk.greenBright("Item ID: " + "    || Product Name: " + "                              || Price: "));
         for (var i = 0; i < result.length; i++) {
-            // console.log("Item ID: " + result[i].item_id + " || Product Name: " + result[i].product_name + " || Price: " + result[i].price);
-            console.log(chalk.yellowBright(result[i].item_id + "            || " + result[i].product_name + "                       || " + result[i].price));
+            // console.log(chalk.yellowBright("Item ID: " + result[i].item_id + " || Product Name: " + result[i].product_name + " || Price: " + result[i].price));
+            // console.log(chalk.yellowBright(result[i].item_id + "            || " + result[i].product_name + "                       || " + result[i].price));
+            console.log(chalk.yellowBright(result[i].item_id + "            || " + result[i].product_name + "                                 || " + result[i].price));
         }
         console.log("\n");
         buyItem();
@@ -57,44 +57,44 @@ function buyItem() {
             {
                 name: "id",
                 type: "input",
-                message: "Enter the ID of the product that you would like to buy: ",
+                message: chalk.yellowBright("Enter the ID of the product that you would like to buy: "),
             },
             {
                 name: "unit",
                 type: "input",
-                message: "Enter the # of units of products that you would like to buy: ",
+                message: chalk.yellowBright("Enter the # of units of products that you would like to buy: "),
             }
         ])
         .then(function (userResponse) {
 
             var userChosenItemID = userResponse.id;
             var userChosenItemCount = userResponse.unit;
-            console.log("User selected Item ID: " + userChosenItemID);
-            console.log("# of units user selected: " + userChosenItemCount);
+            // console.log("User selected Item ID: " + userChosenItemID);
+            // console.log("# of units user selected: " + userChosenItemCount);
 
             var query = "SELECT * FROM products";
 
-            connection.query(query, function (error, result) {
+            db.query(query, function (error, result) {
                 // console.log(result);
 
                 for (var i = 0; i < result.length; i++) {
                     // console.log(result[i].item_id);
                     if (userChosenItemID == result[i].item_id) {
-                        console.log("Item exists");
+                        // console.log("Item exists");
                         var productName = result[i].product_name;
                         var stockQuantity = result[i].stock_quantity;
                         var productPrice = result[i].price;
-                        console.log("Product selected is: " + productName);
-                        console.log("Available quantity is: " + stockQuantity);
-                        console.log("Product price is: " + productPrice);
+                        // console.log("Product selected is: " + productName);
+                        // console.log("Available quantity is: " + stockQuantity);
+                        // console.log("Product price is: " + productPrice);
                         if (stockQuantity < userChosenItemCount) {
-                            console.log("Sorry, availabale quantity of the selected item" + productName + " is only " + stockQuantity);
+                            console.log(chalk.yellowBright("Sorry, Insufficient quantity!. Availabale quantity of the selected item " + productName + " is only " + stockQuantity + ". Please change your order accordingly."));
                         } else {
                             var newStockQuantity = stockQuantity - userChosenItemCount;
                             var totalCost = userChosenItemCount * productPrice;
                             console.log("Total cost is: " + totalCost);
                             console.log("Updated stock quantity is: " + newStockQuantity);
-                            connection.query(
+                            db.query(
                                 "UPDATE products SET ? WHERE ?",
                                 [
                                     {
@@ -105,8 +105,8 @@ function buyItem() {
                                     }
                                 ],
                                 function (error) {
-                                    if (error) throw err;
-                                    console.log("Order placed successfully! Your total cost is: " + totalCost);
+                                    if (error) throw error;
+                                    console.log(chalk.yellowBright("Order placed successfully! Your total cost is: " + totalCost));
 
                                 }
                             );
